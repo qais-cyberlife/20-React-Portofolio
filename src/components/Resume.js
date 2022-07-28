2dfd0ab85ff8c51c4b982d2984f221f0f22581d2
Kais Al Balkhi <qaiscyberlife@gmail.com>
2022-07-28 04:08:04 -0400

    final touches done, ready to submit

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,6 +7,44 @@ function Resume() {
         <>
             <div>
                 <a class="resume-link" href="https://drive.google.com/file/d/1PV7wfPnf7a0gDkZ0NpZ3-Yq2vmIdjiAq/view">Click at the link to Download the Resume</a>
+                <h1>Skills:</h1>
+                <ul>
+                    <li>
+                        HTML
+                    </li>
+
+                    <li>
+                        CSS
+                    </li>
+
+                    <li>
+                        Java
+                    </li>
+
+                    <li>
+                        SQL
+                    </li>
+
+                    <li>
+                        NoSQL
+                    </li>
+
+                    <li>
+                        Insomnia
+                    </li>
+
+                    <li>
+                        MongoDB
+                    </li>
+
+                    <li>
+                        Databases
+                    </li>
+
+                    <li>
+                        React.js
+                    </li>
+                </ul>
             </div>
         </>
     );
